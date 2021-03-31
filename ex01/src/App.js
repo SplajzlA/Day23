af5454e9ca0883b516219ebcff750fbdccb10b03
SplaY <mirza_messic@hotmail.com>
2021-03-31 10:47:15 +0200

    React

---
+++ ex01/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Plant from './Plant';
+
+class App extends Component{
+  render() {
+    return(
+      <div id="App">
+        <Plant />
+      </div>
+    )
+  }
+}
+
+export default App;
